9307636fa6c3e75e3dfddc951033de445f4615bf
lackmannicholas <lackmannicholas@gmail.com>
2023-06-29 00:27:22 -0700

    adding amplify auth

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,12 +4,40 @@ import './App.css';
 import List from './components/List';
 import Add from './components/Add';
 import { useState, useEffect } from 'react';
+import { Auth, Hub } from 'aws-amplify';
 import { updateFeatureFlag, getFeatureFlags, addFeatureFlag, deleteFeatureFlag } from './services';
-
 
 function App() {
   const [featureFlags, setFeatureFlags] = useState([]);
   const [newFeatureFlag, setNewFeatureFlag] = useState();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          getUser().then(userData => setUser(userData));
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+          break;
+      }
+    });
+
+    getUser().then(userData => setUser(userData));
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then(userData => userData)
+      .catch(() => Auth.federatedSignIn());
+  }
+
 
   useEffect(() => {
     const getFF = async () => {
@@ -36,6 +64,12 @@ function App() {
 
   return (
     <div className="App">
+       <p>User: {user ? JSON.stringify(user.attributes) : 'None'}</p>
+      {user ? (
+        <button onClick={() => Auth.signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => Auth.federatedSignIn()}>Federated Sign In</button>
+      )}
         <header className="App-header">
           <img src={logo} className="App-MainLogo" alt="logo" />
         </header>
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure({
+  Auth: {
+      region: awsExports.REGION,
+      userPoolId: awsExports.USER_POOL_ID,
+      userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID,
+      mandatorySignIn: true,
+      oauth: {
+        domain: 'feature-flag-platform-dev.auth.us-east-1.amazoncognito.com',
+        scope: ['email', 'openid', 'profile', 'phone', 'aws.cognito.signin.user.admin'],
+        redirectSignIn: 'http://localhost:3000',
+        redirectSignOut: 'http"//localhost:3000',
+        responseType: 'code',
+      },
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
